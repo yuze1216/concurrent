242fa02be4e18e9e4170dca3a718627c28029d15
yuze <1394975128@qq.com>
2022-02-12 09:37:01 +0800

    save

--- src/concurrent/BoundedQueue.java
+++ src/concurrent/BoundedQueue.java
@@ -55,4 +55,30 @@ public class BoundedQueue<T> {
             lock.unlock();
         }
     }
+    public static void main(String[] args) {
+        BoundedQueue queue = new BoundedQueue(10);
+        Thread t1 = new Thread(()->{
+            for (int i = 0; i < 100; i++) {
+                try {
+                    queue.add(i);
+                    System.out.println("塞入" + i);
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread t2 = new Thread(()->{
+            for ( ; ;){
+                try {
+                    System.out.println("消费" + queue.remove());
+                    Thread.sleep(800);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+    }
 }
